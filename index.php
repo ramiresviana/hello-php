4f060c5f6b76398268aa5d875af6ccde0c89756c
Ramires Viana <59319979+ramiresviana@users.noreply.github.com>
2020-06-09 12:47:24 +0000

    add: static article listing

--- index.php
+++ index.php
@@ -2,4 +2,38 @@
 
 require('functions.php');
 
-echo hello();
+$articles = getArticles();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello HTML - Index</title>
+    <link rel="stylesheet" href="styles.css"/>
+</head>
+
+<body>
+    <header>
+        <h1>Hello HTML</h1>
+    </header>
+    <main>
+<?php foreach($articles as $article): ?>
+        <a href="#">
+            <article>
+                <img src="<?= $article['image'] ?>">
+                <div>
+                    <h2><?= $article['title'] ?></h2>
+                    <p><?= $article['content'] ?></p>
+                </div>
+            </article>
+        </a>
+<?php endforeach ?>
+    </main>
+</body>
+
+</html>
