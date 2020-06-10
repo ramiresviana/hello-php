5e46497c615d0f5b704cca02c557fa267b400f91
Ramires Viana <59319979+ramiresviana@users.noreply.github.com>
2020-06-10 14:36:37 +0000

    fix: page titles

--- edit.php
+++ edit.php
@@ -25,7 +25,7 @@ if ($article == null) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hello HTML - New</title>
+    <title>Hello HTML - Edit</title>
     <link rel="stylesheet" href="styles.css" />
 </head>
 
--- logout.php
+++ logout.php
@@ -20,7 +20,7 @@ if ($_POST) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hello HTML - Login</title>
+    <title>Hello HTML - Logout</title>
     <link rel="stylesheet" href="styles.css" />
 </head>
 
